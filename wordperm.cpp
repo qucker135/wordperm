17cca3d1a81481c36d43c95a0f45390ebe065bc6
qucker <qucker135@gmail.com>
2020-09-15 23:55:11 +0200

    Translate code to english

--- wordperm.cpp
+++ wordperm.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-//#include <stdio.h>
 #include <string>
 using namespace std;
 
@@ -23,7 +22,7 @@ void rightrotate(string& s,int ind,int i){
 	}	
 }
 
-bool npowt(string s,int ind,int i)//auxiliary
+bool nrpt(string s,int ind,int i)//auxiliary
 {
 	for(int it=ind;it<i;it++){
 		if(s[it]==s[i]) return false;
@@ -37,7 +36,7 @@ void perm(string s,int ind=0){
 	}
 	else{
 		for(int i=ind;i<s.length();i++){
-			if(i==ind || npowt(s,ind,i)){
+			if(i==ind || nrpt(s,ind,i)){
 				rightrotate(s,ind,i);
 				perm(s,ind+1);
 				leftrotate(s,ind,i);
@@ -46,29 +45,19 @@ void perm(string s,int ind=0){
 	}
 }
 
-string napis;
+string str;
 
 int main(int argc,char* argv[]){
 	if(argc==1){
-		cout<<"Wpisz napis: ";cin>>napis;
-		perm(napis);
+		cout<<"Type sth: ";cin>>str;
+		perm(str);
 	}
 	else{
 		for(int i=1;i<argc;i++){
-			napis =	std::string(argv[i]); 
-			perm(napis);
+			str =	std::string(argv[i]); 
+			perm(str);
 		}
 	}
-	//cout<<argc<<endl;
-
-	/*
-	for(int i=0;i<argc;i++){
-		cout<<argv[i]<<endl;
-	}
-	*/
-	//swap(napis,0,1);	
-
-	//cout<<napis<<endl;
 	return 0;
 }
 
